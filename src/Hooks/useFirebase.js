30e9de8dad6a013c2410c49c66c856904ab236ae
Yea-sin <yeasinn2002@gmail.com>
2021-12-07 15:46:47 +0600

    final push

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -75,7 +75,7 @@ const useFirebase = () => {
   };
 
   useEffect(() => {
-    fetch(`http://localhost:5000/users/${user.email}`)
+    fetch(`https://glacial-beach-45386.herokuapp.com/users/${user.email}`)
       .then((res) => res.json())
       .then((data) => setAdmin(data.admin));
   }, [user.email]);
@@ -94,7 +94,7 @@ const useFirebase = () => {
 
   const saveUser = (email, displayName, method) => {
     const user = { email, displayName };
-    fetch("http://localhost:5000/users", {
+    fetch("https://glacial-beach-45386.herokuapp.com/users", {
       method: method,
       headers: {
         "content-type": "application/json",
--- src/Pages/MakeAdmin/MakeAdmin.js
+++ src/Pages/MakeAdmin/MakeAdmin.js
@@ -14,7 +14,7 @@ const MakeAdmin = () => {
 
   const handleSubmit = (e) => {
     const user = { email };
-    fetch("http://localhost:5000/users/admin", {
+    fetch("https://glacial-beach-45386.herokuapp.com/users/admin", {
       method: "PUT",
       headers: {
         "content-type": "application/json",
